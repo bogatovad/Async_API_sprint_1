48dc5f6f7e943aeff2a050251348816dd4390327
rvetoshk <roman.vetoshko@megafon.ru>
2023-01-12 23:01:30 +0400

    Flake8 fixes

--- src/models/film.py
+++ src/models/film.py
@@ -2,9 +2,11 @@ import orjson
 
 from pydantic import BaseModel
 
+
 def orjson_dumps(v, *, default):
     """orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем"""
     return orjson.dumps(v, default=default).decode()
+
 
 class Film(BaseModel):
     id: str
