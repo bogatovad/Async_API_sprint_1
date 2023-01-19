008cc5b34538dde1d371b9bf1fa542562083fdc1
rvetoshk <roman.vetoshko@megafon.ru>
2023-01-19 08:49:33 +0400

    Update genres.py route descriptions

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -1,16 +1,19 @@
 from http import HTTPStatus
+from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
-
 from models.api.genre import Genre
-from models.services.genre import GenreId
 from services.film import GenreService, get_genre_service
-from typing import List
 
 router = APIRouter()
 
 
-@router.get('/{genre_id}', response_model=Genre)
+@router.get(
+    '/{genre_id}',
+    response_model=Genre,
+    description='Получить информацию о жанре',
+    response_description='Подробная информация о жанре'
+)
 async def genre_details(genre_id: str, genre_service: GenreService = Depends(get_genre_service)) -> Genre:
     genre = await genre_service.get_by_id(genre_id)
     if not genre:
@@ -18,8 +21,13 @@ async def genre_details(genre_id: str, genre_service: GenreService = Depends(get
     return genre
 
 
-@router.get('/', response_model=List[GenreId])
-async def list_genres(genre_service: GenreService = Depends(get_genre_service)) -> List[GenreId]:
+@router.get(
+    '/',
+    response_model=List[Genre],
+    description='Список жанров',
+    response_description='Список жанров'
+)
+async def list_genres(genre_service: GenreService = Depends(get_genre_service)) -> List[Genre]:
     genres = await genre_service.get_list()
     if not genres:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genres not found')
