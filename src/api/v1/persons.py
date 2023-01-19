7a217cee481cbce0583e7cba322ed25a4a059d9b
rvetoshk <roman.vetoshko@megafon.ru>
2023-01-19 08:49:47 +0400

    Update persons.py route descriptions

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -1,19 +1,23 @@
 from http import HTTPStatus
+from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
-
 from models.api.film import Film
-from models.api.person import PersonFull
-from models.services.genre import GenreId
+from models.api.person import PersonDescription
 from services.film import PersonService, get_person_service
-from typing import List
 
 router = APIRouter()
 
 
-@router.get('/search', response_model=List[PersonFull])
-async def search_persons(query: str, page: int, size: int, person_service: PersonService = Depends(get_person_service)) -> \
-        List[PersonFull]:
+@router.get(
+    '/search',
+    response_model=List[PersonDescription],
+    description='Поиск по персоне',
+    response_description='Результат поиска'
+)
+async def search_persons(
+        query: str, page: int, size: int, person_service: PersonService = Depends(get_person_service)
+) -> List[PersonDescription]:
     print('QUERY!', query, page)
     persons = await person_service.search_persons(query, page, size)
     if not persons:
@@ -21,19 +25,32 @@ async def search_persons(query: str, page: int, size: int, person_service: Perso
     return persons
 
 
-@router.get('/{person_id}', response_model=PersonFull)
-async def person_details(person_id: str, person_service: PersonService = Depends(get_person_service)) -> PersonFull:
+@router.get(
+    '/{person_id}',
+    response_model=PersonDescription,
+    description='Получить информацию о персоне',
+    response_description='Подробная информация о персоне'
+)
+async def person_details(
+        person_id: str, person_service: PersonService = Depends(get_person_service)
+) -> PersonDescription:
     person = await person_service.get_by_id(person_id)
     if not person:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
     return person
 
 
-@router.get('/{person_id}/film', response_model=List[Film])
-async def list_film_by_person(person_id: str, person_service: PersonService = Depends(get_person_service)) -> List[Film]:
+@router.get(
+    '/{person_id}/film',
+    response_model=List[Film],
+    description='Получить фильмы по персоне.',
+    response_description='Фильмы по персоне.'
+
+)
+async def list_film_by_person(
+        person_id: str, person_service: PersonService = Depends(get_person_service)
+) -> List[Film]:
     films = await person_service.get_film_by_id(person_id)
     if not films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films with person not found')
     return films
-
-
