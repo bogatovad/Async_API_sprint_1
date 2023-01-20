bba83e7526e4ede395aec1bc913623291d807b14
bogatovad <githubogatov@yandex.ru>
2023-01-20 13:35:54 +0300

    add description urls.

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -10,7 +10,12 @@ from typing import List
 router = APIRouter()
 
 
-@router.get('/{genre_id}', response_model=Genre)
+@router.get(
+    '/{genre_id}',
+    response_model=Genre,
+    description='Получить информацию о жанре',
+    response_description='Подробная информация о жанре'
+)
 async def genre_details(genre_id: str, genre_service: GenreService = Depends(get_genre_service)) -> Genre:
     genre = await genre_service.get_by_id(genre_id)
     if not genre:
@@ -18,7 +23,12 @@ async def genre_details(genre_id: str, genre_service: GenreService = Depends(get
     return genre
 
 
-@router.get('/', response_model=List[GenreId])
+@router.get(
+    '/',
+    response_model=List[GenreId],
+    description='Список жанров',
+    response_description='Список жанров'
+)
 async def list_genres(genre_service: GenreService = Depends(get_genre_service)) -> List[GenreId]:
     genres = await genre_service.get_list()
     if not genres:
