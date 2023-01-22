68d059d6ada495257597140c568ae6319adf12b0
rvetoshk <roman.vetoshko@megafon.ru>
2023-01-22 20:10:45 +0400

    Use settings from Settings instance instead core.config constants

--- src/core/config.py
+++ src/core/config.py
@@ -11,23 +11,6 @@ env.read_env()
 
 logging_config.dictConfig(LOGGING)
 
-PROJECT_NAME = env('PROJECT_NAME', 'movies')
-
-REDIS_HOST = env('REDIS_HOST', 'redis')
-REDIS_PORT = env('REDIS_PORT', 6379)
-
-ELASTIC_HOST = env('ELASTIC_HOST', 'elasticsearch')
-ELASTIC_PORT = env('ELASTIC_PORT', 9200)
-
-FASTAPI_HOST = env("FASTAPI_HOST", "0.0.0.0")
-FASTAPI_PORT = env("FASTAPI_PORT", 8000)
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
-GENRE_CACHE_EXPIRE_IN_SECONDS = 60 * 5
-PERSON_CACHE_EXPIRE_IN_SECONDS = 60 * 5
-
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = env('PROJECT_NAME', 'movies')
@@ -46,6 +29,7 @@ class Settings(BaseSettings):
     PERSON_CACHE_EXPIRE_IN_SECONDS: int = 60 * 5
 
     class Config:
+        BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         env_file = os.path.join(BASE_DIR, 'enviromens/.env')
 
 
