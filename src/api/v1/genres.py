1ca2a9ce8536a7b13d936653ae263c35ce41fde9
Seniacat <senya.sidorovich@gmail.com>
2023-01-23 00:10:16 +0300

    added error responses

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
-from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
+
+from core.messages import ErrorResponse
 from models.api.genre import GenreResponse
 from services.genre import GenreService, get_genre_service
 
@@ -17,18 +18,18 @@ router = APIRouter()
 async def genre_details(genre_id: str, genre_service: GenreService = Depends(get_genre_service)) -> GenreResponse:
     genre = await genre_service.get_by_id(genre_id)
     if not genre:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genres not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=ErrorResponse.GENRE_NOT_FOUND)
     return GenreResponse(uuid=genre.id, name=genre.name)
 
 
 @router.get(
     '/',
-    response_model=List[GenreResponse],
+    response_model=list[GenreResponse],
     description='Список жанров',
     response_description='Список жанров'
 )
-async def list_genres(genre_service: GenreService = Depends(get_genre_service)) -> List[GenreResponse]:
+async def list_genres(genre_service: GenreService = Depends(get_genre_service)) -> list[GenreResponse]:
     genres = await genre_service.get_list()
     if not genres:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genres not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=ErrorResponse.GENRES_NOT_FOUND)
     return [GenreResponse(uuid=genre.id, name=genre.name) for genre in genres]
---
+++ src/core/messages.py
@@ -0,0 +1,7 @@
+class ErrorResponse:
+
+    FILM_NOT_FOUND = 'film not found'
+    GENRE_NOT_FOUND = 'genre not found'
+    GENRES_NOT_FOUND = 'genres not found'
+    PERSON_NOT_FOUND = 'person not found'
+    PERSON_FILMWORK_NOT_FOUND = 'films with person not found'
