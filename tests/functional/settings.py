89885ec2e702cb73039f9856464b4d261ee51b8a
rvetoshk <roman.vetoshko@megafon.ru>
2023-01-25 23:39:47 +0400

    Add tests settings

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -2,13 +2,10 @@ from pydantic import BaseSettings, Field
 
 
 class TestSettings(BaseSettings):
-    es_host: str = Field('http://127.0.0.1:9200', env='ELASTIC_HOST')
-    es_index: str # TODO to fill
-    es_id_field: str # TODO to fill
-    es_index_mapping: dict # TODO to fill
-
-    redis_host: str # TODO to fill
-    service_url: str # TODO to fill
+    ES_HOST: str = Field("127.0.0.1", env="ELASTIC_HOST")
+    ES_PORT: str = Field("9200", env="ELASTIC_PORT")
+    REDIS_HOST: str = Field("127.0.0.1", env="REDIS_HOST")
+    REDIS_PORT: str = Field("6379", env="REDIS_PORT")
 
 
 test_settings = TestSettings()
